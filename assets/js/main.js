91fb7ba3353a1a799baa9b0c0ade0a2827029090
ekinbukulmez <22412569@student.uwa.edu.au>
2019-02-24 20:12:00 +0800

    changing the whole websiteA

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("#more").click(function() {
-        $('html', 'body').animate({
-            scrollTop: $("#about").offset().top
-        }, 2000);
-    });
-
-});
